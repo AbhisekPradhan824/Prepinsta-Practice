26506caa67d2dd84ed2ac321664f133b5b7f4d2d
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-17 23:13:06 +0530

    Create PositiveorNegative02.java

---
+++ Day-1/Positive-or-negative-number/PositiveorNegative02.java
@@ -0,0 +1,23 @@
+package prepinsta;
+
+import java.util.Scanner;
+
+public class PositiveorNegative02 {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		
+		System.out.println("Enter The Number");
+		int number=scn.nextInt();
+		
+		if(number > 0) 
+			System.out.println("Positive Number..");
+		else if (number==0) 
+			System.out.println("Number is Zero..");
+		else 
+			System.out.println("Negative Number..");
+		
+
+	}
+
+}
