de425912f38c457d2459e1eb2e348b94cd647896
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-18 23:14:11 +0530

    Create GreatestofTwoNumbersUsingTernary.java

---
+++ Day-2/Greatest-of-Two-Numbers/GreatestofTwoNumbersUsingTernary.java
@@ -0,0 +1,24 @@
+package prepinsta;
+
+import java.util.Scanner;
+
+public class GreatestofTwoNumbersUsingTernary {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		
+		System.out.println("Enter First Number:  ");
+		int num1=scn.nextInt();
+		
+		System.out.println("Enter Second Number: ");
+		int num2=scn.nextInt();
+		
+		int greatestNum= num1>num2 ? num1:num2;
+		
+		System.out.println(greatestNum + " Is the Greatest Number..");
+		
+		scn.close();
+
+	}
+
+}
