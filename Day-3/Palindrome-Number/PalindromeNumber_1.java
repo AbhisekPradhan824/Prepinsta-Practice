ee43298a7653b24933f8fe8fe78e13117bd5de24
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-20 00:46:14 +0530

    Create PalindromeNumber_1.java

---
+++ Day-3/Palindrome-Number/PalindromeNumber_1.java
@@ -0,0 +1,37 @@
+package prepinsta;
+
+import java.util.Scanner;
+
+public class PalindromeNumber_1 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("Enter the number");
+		int num=scn.nextInt();
+		
+		int reversed=getReversed(num);
+		
+		if (reversed==num) {
+			System.out.println("The Number " + num+ " Is a Palindrome Number");
+		}else {
+			System.out.println("The Number " + num+ " Is not a Palindrome Number");
+		}
+		
+		scn.close();
+		
+
+	}
+	static int getReversed(int num) {
+		int reversed=0;
+		if(num==0) {
+			return num;
+		}
+		while(num!=0) {
+			reversed= reversed*10 + num%10;
+			num/=10;
+		}
+		return reversed;
+	}
+
+}
