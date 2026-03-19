9e420055a1d56eb93cc695ff550e075b0e8388e3
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-19 21:54:27 +0530

    Create PrimeNumbersInRange_3.java

---
+++ Day-3/Prime-Numbers-In-Range/PrimeNumbersInRange_3.java
@@ -0,0 +1,39 @@
+package prepinsta;
+
+import java.util.Scanner;
+
+public class PrimeNumbersInRange_3 {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		
+		System.out.println("Enter First Number");
+		int first= scn.nextInt();
+		
+		System.out.println("Enter Last Number");
+		int last = scn.nextInt();
+		
+		for(int i=first; i<=last; i++ ) {
+			if(isPrime(i)) {
+				System.out.print(i+ "\t");
+			}
+		}
+		System.out.println();
+		
+		scn.close();
+		
+
+	}
+	static boolean isPrime(int num) {
+		 if(num<2) {
+			 return false;
+		 }
+		 for(int i=2; i<Math.sqrt(num); i++) {
+			 if(num%i==0) {
+				 return false;
+			 }
+		 }
+		 return true;
+	}
+
+}
