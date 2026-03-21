f700f4f457b43f4c9b0ba7025e64b8903519043e
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-21 21:04:59 +0530

    Create FactorsofaNumber_2.java

---
+++ Day-5/Factors of a Number/FactorsofaNumber_2.java
@@ -0,0 +1,24 @@
+package prepinsta;
+
+import java.util.Scanner;
+
+public class FactorsofaNumber_2 {
+
+	public static void main(String[] args) {
+Scanner scn=new Scanner(System.in);
+		
+		System.out.println("Enter the Number: ");
+		int n=scn.nextInt();
+		System.out.print(1+ " , ");
+		
+		for(int i=2; i<=n/2; i++) {
+			if(n%i==0) {
+				System.out.print( i+" , ");
+			}
+		}
+		System.out.println();
+		scn.close();
+
+	}
+
+}
