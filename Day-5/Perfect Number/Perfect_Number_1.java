f4eec5b37d7d8ba7c9ba5617797d7d76c28002d1
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-22 10:32:33 +0530

    Create Perfect_Number_1.java

---
+++ Day-5/Perfect Number/Perfect_Number_1.java
@@ -0,0 +1,29 @@
+package prepinsta;
+
+import java.util.Scanner;
+
+public class Perfect_Number_1 {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		
+		System.out.println("Enter the Number: ");
+		int number=scn.nextInt();
+		int sum=0;
+		
+		for(int i=1; i<number; i++) {
+			if(number%i==0) {
+				sum+=i;
+			}
+		}
+
+		if(number==sum) {
+			System.out.println("The number "+ number + " is a Perfect Number");
+		}else {
+			System.out.println("The number "+ number + " is not a Perfect Number");
+		}
+		
+		scn.close();
+	}
+
+}
