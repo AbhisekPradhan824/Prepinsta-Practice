cc8821ab8415c9dfe9354c6b7c2c57a2e272dc93
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-03-23 12:54:24 +0530

    Create LCM_1.java

---
+++ Day-7/LCM/LCM_1.java
@@ -0,0 +1,42 @@
+package prepinsta;
+
+import java.util.Scanner;
+
+public class LCM_1 {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		
+		System.out.println("Enter First Number");
+		int num1=scn.nextInt();
+		
+		System.out.println("Enter Second Number");
+		int num2=scn.nextInt();
+		
+		int lcm=getLCM(num1, num2);
+		
+		System.out.println("LCM : "+ lcm);
+		scn.close();
+		
+		
+	}
+	static int getLCM(int num1, int num2) {
+		
+		int max=Math.max(num1, num2);
+    int lcm=0;
+		
+		if(num1==num2) {
+			return num1;
+		}
+		if(num2%num1==0) {
+			return num2;
+		}
+		for(int i=max; i<=num1*num2; i++) {
+			if(i%num1==0 && i%num2==0) {
+				lcm=i;
+        break;
+			}
+		}
+		return lcm;
+	}
+}
