4985e8652d2d726418655f10960b137242949c83
Abhisek Pradhan <125997876+AbhisekPradhan824@users.noreply.github.com>
2026-04-05 22:15:40 +0530

    Create HCF_Recursion_01.java

---
+++ Day-13/HCF using Recursion/HCF_Recursion_01.java
@@ -0,0 +1,34 @@
+package prepinsta;
+
+import java.util.Scanner;
+
+public class HCF_Recursion_01 {
+	public static void main(String[] args) {
+		Scanner scn =new Scanner(System.in);
+		System.out.println("Enter the first Number");
+		int num1 =scn.nextInt();
+		System.out.println("Enter second Number");
+		int num2 = scn.nextInt();
+		
+		int hcf =getHcf(num1, num2);
+		System.out.println("HCF is : "+hcf);
+		scn.close();
+	}
+	public static int getHcf(int num1, int num2) {
+		if(num1==0) {
+			return num2;
+		}
+		if(num2==0) {
+			return num1;
+		}
+		if(num1==num2) {
+			return num1;
+		}
+		if(num1> num2) {
+			return getHcf(num1-num2, num2);
+		}else {
+			return getHcf(num2, num2-num1);
+		}
+	}
+
+}
